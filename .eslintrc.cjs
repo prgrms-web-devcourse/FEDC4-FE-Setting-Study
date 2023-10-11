f42bd9eeab16084914a7e2e094a9c33c00e1e0a5
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-10-11 15:03:17 +0900

    chore: lint필요한 plugin설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,115 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:unicorn/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react-refresh',
+    'prettier',
+    'eslint-plugin-prettier',
+    'eslint-plugin-react',
+    'import'
+  ],
   rules: {
+    // TODO : 마지막에 불필요한 console지울 때 켜주시면 console부분만 빨간색으로 변해서 유용할 것 같습니다
+    // 'no-console': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        selector: 'variable',
+        leadingUnderscore: 'allow'
+      },
+      {
+        format: ['camelCase', 'PascalCase'],
+        selector: 'function'
+      },
+      {
+        format: ['PascalCase'],
+        selector: 'interface'
+      },
+      {
+        format: ['PascalCase'],
+        selector: 'typeAlias'
+      }
+    ],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'always', children: 'always' }
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown'
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: '@hooks/*',
+            group: 'internal',
+            position: 'after'
+          },
+          {
+            pattern: '@pages/*',
+            group: 'internal',
+            position: 'after'
+          },
+          {
+            pattern: '@components/*',
+            group: 'internal',
+            position: 'after'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['@tanstack*'],
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ],
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true
+        }
+      }
+    ]
+  }
 }
