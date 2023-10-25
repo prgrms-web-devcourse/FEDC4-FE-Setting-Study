b27645154866691a5339845d4d5504d08488fe29
Eugene Kim <eugene02876@gmail.com>
2023-10-25 16:33:11 +0900

    chore: 스토리북 배포 테스트 1트

--- src/components/common/Button/Button.stories.ts
+++ src/components/common/Button/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+import  Button from './Button';
 
 const meta = {
   title: 'Example/Button',
@@ -42,4 +42,4 @@ export const Small: Story = {
     size: 'small',
     label: 'Button',
   },
-};
+}
