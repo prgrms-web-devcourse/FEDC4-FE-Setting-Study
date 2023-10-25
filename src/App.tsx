92d17b57892d8caae17ef6f9e0ef2d1a541a735f
Eugene Kim <eugene02876@gmail.com>
2023-10-25 16:39:51 +0900

    chore: App.jsx 오타 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import { Button } from '@/components/common'
+
+import Button from '@/components/common/Button/Button'
 import './style/global.css'
 
 // TODO : 절대경로 test
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  // ❗️TODO : 연습용 레포라서 에러들을 몇 개 내보았습니다!
-  // const array_function = (number) => number
-  // const myNumber = 10
-  // const my_number = 20 // 얘 에러남!
-  // const mynumber = 20
   return (
     <>
         <Button label='안녕' primary={true}></Button>
